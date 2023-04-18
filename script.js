6daf0758fdc618e26bdf8648181dae946967e056
Alexandra <a.kuznetsova1992@bk.ru>
2023-04-18 12:41:55 +0300

    Домашнее задание за урок 19

--- script.js
+++ script.js
@@ -1,138 +1,111 @@
 'use strict';
-// function calculateArea(r){
-//     let area;
-//     if(r<=0){
-//         return 0;
-//     }else{
-//         area = Math.PI * r *r;
-//         return area;
-//     }
-// }
-// let radius = 2.2;
-// let theArea = calculateArea(radius);
-// console.log('The area is: ' + theArea);
 
-// Дана строка 'abc!def!'. Замените все ! на '@' 
-// с помощью глобального поиска и замены.
-// let txt = 'abc!def!';
-// console.log(txt.replace(/!/g, '@'));
+//Домашнее задание
 
 
-// Дана строка 'aaa bbb ccc'. Вырежите из нее слово 'bbb' 
-// тремя разными способами (через substr, substring, slice).
-// let txt = 'aaa bbb ccc';
-// console.log(txt.substr(4,3));
-// console.log(txt.substring(4,7));
-// console.log(txt.slice(4,7));
+// Создайте объект city1 (var city1 = {}), укажите у него свойства name 
+// (название города, строка) со значением «ГородN» и population 
+// (населенность города, число) со значением 10 млн.
 
-// Дана строка 'js'. Сделайте из нее строку 'JS'.
-// let txt = 'js';
-// console.log(txt.toUpperCase());
-
-// Дана строка 'я люблю JS!'. Найдите количество 
-// символов в этой строке.
-// let txt = 'я люблю JS!';
-// console.log(txt.length);
-
-// Домашнее задание
-// Выписать в комментариях. Вам необходимо найти все переменные,
-// функции, аргументы и параметры.
-// Запишите имена в соответсвующие пункты
-// Переменные: years, myDog, guests;
-// Функции: dogYears, makeTea, secret;
-// Встроенные функции: ??
-// Аргументы: myDog, 4, guests, 'Earl Grey';
-// Параметры: dogname, age, cups, tea;
-// function dogYears(dogname, age){
-//     let years = age * 2;
-//     console.log(dogName + ' is ' + years + ' years old');
-// }
-// let myDog = 'Rex';
-// dogYears(myDog, 4);
-
-// function makeTea (cups, tea){
-//     console.log('Breawing ' + cups + ' cups of' + tea);
-// }
-// let guests = 2;
-// makeTea(guests, 'Earl Grey');
-
-// function secret(){
-//     console.log('The secret of life is 28');
-// }
-// secret();
-
-
-// Дана строка 'JS'. Сделайте из нее строку 'js'.
-
-let text = 'JS';
-console.log(text.toLowerCase());
-
-
-// Дана строка 'я люблю JS!'. Вырежите из нее слово 
-// 'люблю' и слово 'javascript' тремя разными способами 
-// (через substr, substring, slice).
-
-text = 'я люблю javascript!';
-console.log(text.substr(2, 5));
-console.log(text.substr(8, 10));
-console.log(text.substring(2, 7));
-console.log(text.substring(8, 18));
-console.log(text.slice(2, 7));
-console.log(text.slice(8, 18));
-
-
-
-// Дана строка 'я люблю JS!'. Найдите позицию 
-// подстроки 'люблю'.
-
-text = 'я люблю JS!';
-console.log(text.indexOf('люблю'));
-
-// Дана переменная txt, в которой хранится какой-либо текст. 
-// Реализуйте обрезание длинного текста по следующему 
-// принципу: если количество символов этого текста больше 
-// заданного в переменной n, то в переменную result запишем 
-// первые n символов строки txt и добавим в конец 
-// троеточие '...'. В противном случае в переменную 
-// result запишем содержимое переменной txt.
-
-
-function cutText(n) {
-    let result;
-    let txt = "О, сколько мне открытий чудных дарует Просвещения дух!";
-    if(txt.length < n){
-        result = txt;
-    } else {
-        result = txt.slice(0, n) + "..."
-        return result;      
+var city1 = {
+    name: "ГородN",
+    population: "10 млн",
+    
+    getName() {
+        return(city1.name)
     }
-   
 }
-let symbols = 34;
-let myTxt = cutText(symbols);
-console.log ('что у меня получилось: ' + myTxt)
-//Не разобралась, почему если n>text.length то получается undefined
 
-// Для решения задач данного блока вам понадобятся следующие 
-// методы: replace.
-// Дана строка 'Я-люблю-JS!'. 
-// Замените все  '-' на '!' с помощью глобального 
-// поиска и замены.
+console.log(city1)
 
-let txt = 'Я-люблю-JS!'
-console.log(txt.replace(/-/g, '!'));
+// Создайте объект city2 через нотацию {name: "ГородM", population: 1e6}.
+
+let city2 = {
+    getName() {
+        return(city2.name)
+    }
+} 
+
+city2.name = "ГородM";
+city2.population = 1e6;
+
+console.log(city2)
+
+// Создайте у объектов city1 и city2 методы getName(), которые вернут 
+// соответствующие названия городов.
+
+console.log(city1.getName());
+console.log(city2.getName());
 
 
-// Дана строка 'я люблю JS'. С помощью метода split 
-// запишите каждое слово этой строки в отдельный элемент 
-// массива.
+// Создайте методы exportStr() у каждого из объектов. Этот метод должен 
+// возвращать информацию о городе в формате 
+// «name=ГородN\npopulation=10000000\n». Для второго города будет строка 
+// со своими значениями. Примечание: можно обращаться к каждому свойству 
+// через цикл for/in, но методы объекта возвращать не нужно. 
 
-txt = 'я люблю JS';
-console.log(txt.split(" "));
 
-// Дана строка 'привет мир'. С помощью метода split 
-// запишите каждый символ этой строки в отдельный элемент 
-// массива.
+// Создайте глобальную функцию getObj(), которая возвращает this. 
+// А у каждого из объектов city1 или city2 метод getCity, который 
+// ссылается на getObj. Проверьте работу метода. Примечание: к объекту 
+// вызова можно обратиться через this..
 
-txt = 'привет мир';
-console.log(txt.split(""));
+// <= Вот эту часть задания я пока не поняла
+
+
+
+// Создайте массив d1 с числовыми величинами 45,78,10,3.  
+// Добавьте в массив d1 еще одно число ( d1[7] = 100). 
+// Выведите в консоль весь массив и его элементы с индексами 6 и 7.
+
+let d1 = [45, 78, 10, 3];
+
+d1[7] = 100;
+
+console.log(d1);
+console.log(d1[6]);
+console.log(d1[7]);
+
+
+// Создайте массив d2 с числовыми величинами 45,78,10,3. 
+// Посчитайте в переменную sum2 сумму чисел в нем, при помощи цикла for.
+
+let d2 = [45,78,10,3]
+let sum2 = 0;
+
+function count() {
+    for (let i = 0; i < d2.length; i++) {
+        sum2 += d2[i];
+    }
+    return sum2
+}
+count();
+
+console.log(sum2)
+
+
+
+// Создайте массив d3 с числовыми величинами 45,78,10,3. 
+// Добавьте в массив d3 еще одно число (например, d[7] = 100) 
+// Посчитайте в переменную sum3 сумму чисел в нем, при помощи цикла for/in.
+
+let d3 = [45,78,10,3]
+
+d3[7] = 100
+
+console.log(d3)
+
+// Не получилось((
+
+
+// Создайте массив d4 с числовыми величинами 45,78,10,3. 
+// Напишите функцию сортировки my(a,b), которая при вызове d4.sort(my) 
+// отсортирует элементы массива по убыванию чисел. Вызовите d4.sort(my)
+
+let d4 = [45,78,10,3]
+
+d4.sort((a, b) => b - a)
+
+console.log(d4)
+
+// Не по заданию, но ответ ведь верный?))
